55465836c121fafdf740216eb2e3d503f3696fc2
angelperez <angel.per.cho@gmail.com>
2017-01-31 13:08:35 -0400

    feature/FirstCommit structure base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardsKpiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dashboardsKpiApp
+ */
+angular.module('dashboardsKpiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
